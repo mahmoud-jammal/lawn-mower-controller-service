94600d540a992be431f99fd3a51c17412d135c9f
MJ <m.jammal94@gmail.com>
2025-05-22 09:39:57 +0200

    Add happy flow test

--- lawn-mower-controller-service-app/src/test/java/org/example/lawnmowercontrollerservice/integrationtests/LawnMowerCoordinatesIT.java
+++ lawn-mower-controller-service-app/src/test/java/org/example/lawnmowercontrollerservice/integrationtests/LawnMowerCoordinatesIT.java
@@ -1,28 +1,41 @@
 package org.example.lawnmowercontrollerservice.integrationtests;
 
+import org.example.lawnmowercontrollerservice.adapters.vo.requests.UpdateCoordinatesRequestVO;
+import org.example.lawnmowercontrollerservice.adapters.vo.responses.UpdateCoordinatesResponseVO;
 import org.example.lawnmowercontrollerservice.app.LawnMowerControllerServiceApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @SpringBootTest(classes = LawnMowerControllerServiceApp.class,
                 webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class LawnMowerCoordinatesIT {
+class LawnMowerCoordinatesIT {
 
     private static final String UPDATE_MOWERS_COORDINATES_PATH = "/lawn-mower-controller-service/mowers/coordinates";
-    @LocalServerPort
-    private int port;
+    private static final UpdateCoordinatesResponseVO EXPECTED_COORDINATES = new UpdateCoordinatesResponseVO(List.of("1 3 N", "5 1 E"));
 
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
     void updateCoordinatesTest() {
+        Mono<UpdateCoordinatesRequestVO> requestBody =
+                Mono.just(new UpdateCoordinatesRequestVO(List.of("5 5", "1 2 N", "LFLFLFLFF", " ", "3 3 E", " ", "FFRFFRFRRF")));
         webTestClient.put()
-                     .uri("http://localhost:" + port + UPDATE_MOWERS_COORDINATES_PATH)
+                     .uri(UPDATE_MOWERS_COORDINATES_PATH)
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .body(requestBody, UpdateCoordinatesRequestVO.class)
                      .exchange()
-                     .expectBody(String.class);
+                     .expectHeader()
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .expectStatus()
+                     .isOk()
+                     .expectBody(UpdateCoordinatesResponseVO.class)
+                     .isEqualTo(EXPECTED_COORDINATES);
     }
 }
